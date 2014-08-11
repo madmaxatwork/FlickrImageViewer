922309289bafa20b22dcfef6c42d3e2011202e52
madmaxatwork <tejasjukar@gmail.com>
2014-08-11 00:20:37 -0700

    Updated Application Icon

--- gen/com/example/flickrimageviewer/R.java
+++ gen/com/example/flickrimageviewer/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int grid_color_selector=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int images=0x7f020002;
+        public static final int application_launcher=0x7f020000;
+        public static final int grid_color_selector=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int images=0x7f020003;
     }
     public static final class id {
         public static final int caption_date=0x7f0a0004;
